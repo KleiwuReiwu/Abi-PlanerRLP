be500d7045c2b8fa66e33c3accea52e91a4cd9d4
ニック <45759644+KleiwuReiwu@users.noreply.github.com>
2018-12-14 22:16:09 +0100

    Add files via upload

---
+++ Klausur.java
@@ -0,0 +1,23 @@
+public class Klausur {
+
+    int notenpunkte;
+    int halbjahr;
+
+    public int getNotenpunkte() {
+        return notenpunkte;
+    }
+
+    public void setNotenpunkte(int notenpunkte) {
+        this.notenpunkte = notenpunkte;
+    }
+
+    public int getHalbjahr() {
+        return halbjahr;
+    }
+
+    public void setHalbjahr(int halbjahr) {
+        this.halbjahr = halbjahr;
+    }
+
+
+}
---
+++ Muendlich.java
@@ -0,0 +1,23 @@
+public class Muendlich {
+
+    int notenpunkte;
+    int halbjahr;
+
+    public int getNotenpunkte() {
+        return notenpunkte;
+    }
+
+    public void setNotenpunkte(int notenpunkte) {
+        this.notenpunkte = notenpunkte;
+    }
+
+    public int getHalbjahr() {
+        return halbjahr;
+    }
+
+    public void setHalbjahr(int halbjahr) {
+        this.halbjahr = halbjahr;
+    }
+
+
+}
